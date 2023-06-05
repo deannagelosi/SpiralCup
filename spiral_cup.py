49e5bc358b6b0c236a88064a00aed4a552482603
Deanna Gelosi <37255721+deannagelosi@users.noreply.github.com>
2023-06-04 23:31:45 -0600

    new SpiralCup class and fix bump()

--- spiral_cup.py
+++ spiral_cup.py
@@ -1,101 +1,108 @@
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as geom
 import math
-import extruder_turtle
-from extruder_turtle import *
+from extruder_turtle import ExtruderTurtle
 
 # to do
 # - vary spacing between bumps (every bump, second bump, fourth, etc)
 # - dynamically scale the number of bumps
 
-t = ExtruderTurtle() # print lines
+class SpiralCup:
+    def __init__(self):
+        self.t = ExtruderTurtle() # print lines
+        # lines = self.t.get_lines()
 
-def square(layer_height, radius, bump_height, bump_width, offset):
-    v = 1
-    for k in range (0, layer_height):
-        i = 0
-        while i < 360:
-            x = radius * math.cos(math.radians(i))
-            y = radius * math.sin(math.radians(i))
-            t.set_position(x, y)
-            t.lift(0.01)
-            if i==v:
-                # make a bump
-                t.right(90)
-                t.forward(bump_height)
-                t.left(90)
-                r_prime = radius + bump_height
-                for j in range (0, bump_width):
-                    x = r_prime * math.cos(math.radians(i+j))
-                    y = r_prime * math.sin(math.radians(i+j))
-                    t.set_position(x, y)
-                i += bump_width - 1
-                t.left(90)
-                t.forward(bump_height)
-                t.right(90)
-                continue
-            i += 1  
+    def bump(self, layer_height, radius, bump_height, bump_width, offset, shape_type):
+        if shape_type == 0:
+            lines = self.square(layer_height, radius, bump_height, bump_width, offset)
+            return lines 
+        elif shape_type == 1:
+            lines = self.triangle(layer_height, radius, bump_height, bump_width, offset)
+            return lines 
+        elif shape_type == 2:
+            lines = self.loop(layer_height, radius, bump_height, bump_width, offset)
+            return lines 
+        else:
+            print("Invalid shape_type: " + str(shape_type))
+
+    def square(self, layer_height, radius, bump_height, bump_width, offset):
+        v = 1
+        for k in range (0, layer_height):
+            i = 0
+            while i < 360:
+                x = radius * math.cos(math.radians(i))
+                y = radius * math.sin(math.radians(i))
+                self.t.set_position(x, y)
+                self.t.lift(0.01)
+                if i==v:
+                    # make a bump
+                    self.t.right(90)
+                    self.t.forward(bump_height)
+                    self.t.left(90)
+                    r_prime = radius + bump_height
+                    for j in range (0, bump_width):
+                        x = r_prime * math.cos(math.radians(i+j))
+                        y = r_prime * math.sin(math.radians(i+j))
+                        self.t.set_position(x, y)
+                    i += bump_width - 1
+                    self.t.left(90)
+                    self.t.forward(bump_height)
+                    self.t.right(90)
+                    continue
+                i += 1  
+                
+            v += offset
+        lines = self.t.get_lines()
+        return lines
             
-        v += offset
+    def triangle(self, layer_height, radius, bump_height, bump_width, offset):
+        v = 1
+        for k in range (0, layer_height):
+            i = 0
+            while i < 360:
+                x = radius * math.cos(math.radians(i))
+                y = radius * math.sin(math.radians(i))
+                self.t.set_position(x, y)
+                self.t.lift(0.01)
+                if i==v:
+                    # make a bump
+                    self.t.right(60)
+                    self.t.forward(bump_height)
+                    self.t.left(120)
+                    self.t.forward(bump_height)
+                    self.t.right(120)
+                    i += bump_width - 1
+                    continue
+                i += 1
+            v += offset
+        lines = self.t.get_lines()
+        return lines
 
-def triangle(layer_height, radius, bump_height, bump_width, offset):
-    v = 1
-    for k in range (0, layer_height):
-        i = 0
-        while i < 360:
-            x = radius * math.cos(math.radians(i))
-            y = radius * math.sin(math.radians(i))
-            t.set_position(x, y)
-            t.lift(0.01)
-            if i==v:
-                # make a bump
-                t.right(60)
-                t.forward(bump_height)
-                t.left(120)
-                t.forward(bump_height)
-                t.right(120)
-                i += bump_width - 1
-                continue
-            i += 1
-        v += offset
-
-# def bump(layer_height, radius, bump_height, bump_width, offset, shape_type):
-#     if shape_type == 0:
-#         square(layer_height, radius, bump_height, bump_width, offset)
-#     elif shape_type == 1:
-#         triangle(layer_height, radius, bump_height, bump_width, offset)
-#     elif shape_type == 2:
-#         loop(layer_height, radius, bump_height, bump_width, offset)
-#     else:
-#         print(f'Invalid shape_type: {shape_type}')
-
-def loop(layer_height, radius, bump_height, bump_width, offset):
-    v = 1
-    for k in range (0, layer_height):
-        i = 0
-        while i < 360:
-            x = radius * math.cos(math.radians(i))
-            y = radius * math.sin(math.radians(i))
-            t.set_position(x, y)
-            t.lift(0.01)
-            if i==v:
-                # make a bump
-                t.right(45)
-                t.forward(math.sqrt(2) * bump_height)
-                t.right(90)               
-                r_prime = radius + bump_height
-                for j in range (0, bump_width):
-                    x = r_prime * math.cos(math.radians(i-j))
-                    y = r_prime * math.sin(math.radians(i-j))
-                    t.set_position(x, y)
-                i += bump_width + 1
-                t.right(135)
-                t.forward(math.sqrt(2) * bump_height)
-                t.right(90)
-                continue
-            i += 1  
-            
-        v += offset    
-            
-lines = t.get_lines()
-# turtle = t.draw_turtle()
+    def loop(self, layer_height, radius, bump_height, bump_width, offset):
+        v = 1
+        for k in range (0, layer_height):
+            i = 0
+            while i < 360:
+                x = radius * math.cos(math.radians(i))
+                y = radius * math.sin(math.radians(i))
+                self.t.set_position(x, y)
+                self.t.lift(0.01)
+                if i==v:
+                    # make a bump
+                    self.t.right(45)
+                    self.t.forward(math.sqrt(2) * bump_height)
+                    self.t.right(90)               
+                    r_prime = radius + bump_height
+                    for j in range (0, bump_width):
+                        x = r_prime * math.cos(math.radians(i-j))
+                        y = r_prime * math.sin(math.radians(i-j))
+                        self.t.set_position(x, y)
+                    i += bump_width + 1
+                    self.t.right(135)
+                    self.t.forward(math.sqrt(2) * bump_height)
+                    self.t.right(90)
+                    continue
+                i += 1  
+            v += offset
+        lines = self.t.get_lines()
+        return lines
